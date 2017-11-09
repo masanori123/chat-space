9371ba3e2ad1e4d7a54e22f3f703ef8e09bd60d4
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-09 16:10:38 +0900

    グループ作成ボタン
    ボタンのリンク先と新規グループを作る機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,14 +4,23 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(group_params)
+    redirect_to controller: :messages, action: :index
   end
 
   def edit
+    @group = Group.find(group_params[:id])
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name,user_ids: [])
   end
 end
