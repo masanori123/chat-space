0517a2f7d72b3cc6bcebd60d0a5360e1348025a3
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-10 21:14:40 +0900

    グループ編集機能の実装
    ビューの変更
    更新機能の追加
    更新後のルートの指定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,14 +17,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(group_params[:id])
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
-    params.require(:group).permit(:name,user_ids: [])
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
