00b470dcdbb53bc97ebc608af426bb00cd0b4134
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-13 20:49:03 +0900

    リンク先の指定
    ビューを編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,21 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
-  end
-
-  def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new
+    if @message.save(message_params)
+      redirect_to controller: :messages, action: :index
+    else
+      redirect_to group_message_path(params[:group_id])
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image, :created_at).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
 
