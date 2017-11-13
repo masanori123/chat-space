ac329f344709e7458632f063760e90f0d1a26d80
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-13 12:49:37 +0900

    message送信機能
    メッセージの保存とエラー時の実装
    ビューでモデルと関連づけるためにform_forの記述
    Cssの修正
    コントローラでアクションの定義
    アップローダーのインストール
    ミニマジックのインストール

--- Gemfile
+++ Gemfile
@@ -64,3 +64,7 @@ gem 'font-awesome-rails'
 gem 'devise'
 
 gem 'rails-i18n'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,22 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
-  end
-
-  def new
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    binding.pry
+    if @message.save
+      redirect_to controller: :messages, action: :index
+    else
+      redirect_to group_messages_path(params[:group_id])
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :image, :created_at).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
 
