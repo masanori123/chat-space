016294ed8d1d024ea8be293ea14dbed87c958b31
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-15 20:26:07 +0900

    サイドバーにメッセージの表示
    　

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def group_message
+    @groups = current_user.groups
+    @groups.each do |group|
+      if group.messages.present?
+        if group.messages.last.body.present?
+         return group.messages.last.body
+        else
+          return "画像が投稿されています"
+        end
+      else
+       return "まだメッセージはありません"
+      end
+    end
+  end
 end
