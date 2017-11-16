dc6d54576788c5b379fb62567d3af8b3f6a75f02
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-16 12:24:50 +0900

    リファクタリング
    ビューの処理をヘルパーに記述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,13 @@
 module ApplicationHelper
-  def group_message
-    @groups = current_user.groups
-    @groups.each do |group|
-      if group.messages.present?
-        if group.messages.last.body.present?
-         return group.messages.last.body
-        else
-          return "画像が投稿されています"
-        end
+  def group_message(group)
+    if group.messages.present?
+      if group.messages.last.body.present?
+        return group.messages.last.body
       else
-       return "まだメッセージはありません"
+        return "画像が投稿されています"
       end
+    else
+      return "まだメッセージはありません"
     end
   end
 end
