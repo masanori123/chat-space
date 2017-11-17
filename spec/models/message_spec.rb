443c047cc84d14d87effadffa919d021ebbd48a8
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-17 20:25:33 +0900

    画像があれば保存できる
    不要な記述の削除

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 describe Message do
 
   describe '#create' do
-    # it "is invalid without a body" do
-    #   message = build(:message, body: nil, image: nil)
-    #   message.valid?
-    #   binding.pry
-    #   expect(message.errors[:body]).to include("can't be blank")
-    # end
+
     it "is valid with a body " do
       message = build(:message, image: nil)
+      expect(message).to be_valid
+    end
+
+    it "is valid with a image " do
+      message = build(:message, body: nil)
       expect(message).to be_valid
     end
   end
