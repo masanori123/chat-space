45b0d4e051dd95b6a370e317b649f764c184c5d7
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-18 14:58:17 +0900

    group_idが無いと保存できない

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -18,6 +18,20 @@ describe Message do
       expect(message).to be_valid
     end
 
+    # it "is invalid without a group && an image" do
+    #   message = build(:message, body: nil, image: nill:)
+    #   message.valid?
+    #   expect(message.errors[:group, :image]).to include("can't be blank")
+    # end
+
+    it "is invalid without a group" do
+      message = build(:message, group: nil)
+      message.valid?
+      expect(message.errors[:group]).to include("を入力してください")
+    end
+
+
+
 
   end
 end
