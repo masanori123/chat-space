9ecb01e857117a57a2c4eb3758a6bb4bb9416ee1
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-18 17:38:19 +0900

    テストコードを見やすく
    Contextを使って条件ごとに括りを変えた。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,39 +2,41 @@ require 'rails_helper'
 describe Message do
 
   describe '#create' do
-
-    it "is valid with a body " do
+    context 'is valid' do
+      it "is valid with a body " do
       message = build(:message, image: nil)
       expect(message).to be_valid
-    end
+      end
 
-    it "is valid with a image " do
+      it "is valid with a image " do
       message = build(:message, body: nil)
       expect(message).to be_valid
-    end
+      end
 
-    it "is valid with a body && a image " do
+      it "is valid with a body && a image " do
       message = build(:message)
       expect(message).to be_valid
+      end
     end
 
-    it "is invalid without a group && an image" do
+    context 'is invalid' do
+      it "is invalid without a group && an image" do
       message = build(:message, body: nil, image: nil)
       message.valid?
       expect(message.errors[:body]).to include("エラーです")
-    end
+      end
 
-    it "is invalid without a group" do
+      it "is invalid without a group" do
       message = build(:message, group: nil)
       message.valid?
       expect(message.errors[:group]).to include("を入力してください")
-    end
+      end
 
-    it "is invalid without an user" do
+      it "is invalid without an user" do
       message = build(:message, user: nil)
       message.valid?
       expect(message.errors[:user]).to include("を入力してください")
+      end
     end
-
   end
 end
