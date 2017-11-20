3bc6f231ef6d52416f66bdee81830ba72990062b
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-20 17:14:45 +0900

    @messageがちゃんと生成されている

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe MessagesController, type: :controller do
   let(:user) { create(:user) }
   let(:group) { create(:group) }
+  let(:groups) { create(:groups)}
+  let(:message) { create(:message)}
+  let(:messages) { crate(:messages)}
+  let(:group_id) do
+    { prams: { gtoup_id: group.id, message: attributes_for(:message)}}
+  end
 
   describe 'GET #index' do
     let(:params) {{ group_id: group.id }}
@@ -13,6 +19,10 @@ describe MessagesController, type: :controller do
 
     it "assigns the requested contact to @groups" do
       expect(assigns(:group)).to eq group
+    end
+
+    it 'assigns the requested messsage to @message' do
+      expect(assigns(:message)).to be_a_new(Message)
     end
   end
 end
