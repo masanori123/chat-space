d3045dae19d7677267aca3ff100e5887d09fcd37
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-24 18:04:31 +0900

    削除機能の修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -68,6 +68,7 @@ $(function() {
   $(document).on("click", ".chat-group-user__btn--remove", function() {
     var user_id = $(this).data("user_id");
     var user_name = $(this).data("user-name");
-    $('#chat-group-user-8').remove();
+    $(this).remove();
+    $(".chat-group-user__name").remove();
   })
 });
