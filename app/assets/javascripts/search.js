36c6f25eb9fc487e10069dd3fb47a1a7e1997f6d
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-24 17:34:13 +0900

    検索結果からHTMLを消し、チャットメンバーの部分にHTMLを追加する

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -23,6 +23,15 @@ $(function() {
     $("#user-search-result").append(html);
   }
 
+  function appendAddUser(user_id, user_name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    $("#chat-group-users").append(html);
+  }
+
   $("#chat-group-form__input").on("keyup", function() {
     var input = $("#chat-group-form__input").val();
 
@@ -50,8 +59,9 @@ $(function() {
   });
 
   $(document).on("click", ".chat-group-user__btn--add", function() {
-    var user_id = $(".chat-group-user__btn--add").attr("data-user-id");
+    var user_id = $(this).data("user-id");
+    var user_name = $(this).data("user-name");
     $('.chat-group-user').remove();
-
+      appendAddUser(user_id, user_name);
   });
 });
