8e243e5fcab39bea2080c58206eb446af5ff84a3
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-29 20:27:31 +0900

    自動更新の導入

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,11 +5,13 @@ $(function() {
       insertImage = `<img src="${message.image}">`;
     }
     var html = `
-      <div class="chat-main__body--message-list__message">
-        <div class="chat-main__body--message-list__message-name">${message.name}</div>
-        <div class="chat-main__body--message-list__message-time">${message.time}</div>
-        <div class="chat-main__body--message-list__message-body">${message.body}</div>
-        ${insertImage}
+      <div class="chat-main__body--message-list" id="js-messages-list">
+        <div class="chat-main__body--message-list__message" data-message-id="${message.id}">
+          <div class="chat-main__body--message-list__message-name">${message.name}</div>
+          <div class="chat-main__body--message-list__message-time">${message.time}</div>
+          <div class="chat-main__body--message-list__message-body">${message.body}</div>
+          ${insertImage}
+        </div>
       </div>`;
 
     return html;
@@ -18,7 +20,7 @@ $(function() {
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href //+ '/messages'
+    var href = window.location.href
     $.ajax({
       url: href,
       type: "POST",
@@ -29,7 +31,7 @@ $(function() {
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.chat-main__body--message-list').append(html)
+      $('#js-messages-list').append(html)
       var $messages = $('.chat-main__body');
       $messages.animate({
         scrollTop: $messages[0].scrollHeight
@@ -40,4 +42,32 @@ $(function() {
     })
     return false;
   })
+
+  var interval = setInterval(function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    $.ajax({
+      url: location.href,
+      type: "GET",
+      dataType: 'json'
+    })
+    .done(function(json) {
+      console.log(json);
+      var id = $('.chat-main__body--message-list__message').last().data('messageId');
+      console.log(id);
+      var message_id = $(".chat-main__body--message-list").last().data("message-id");
+      console.log(message_id);
+      var insertHTML = '';
+      json.messages.forEach(function(message) {
+        if (message.id > id ) {
+          insertHTML += buildHTML(message);
+        }
+      });
+      $('#js-messages-list').append(insertHTML);
+    })
+    .fail(function(json) {
+       alert('自動更新に失敗しました');
+    });
+  } else {
+    clearInterval(interval);
+   }} , 5000 );
 });
